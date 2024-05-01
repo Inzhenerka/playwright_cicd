f4357aa11c4a55fd5c8a563182329620558218cf
Dmitry Eremin <mitya1705@gmail.com>
2024-05-02 01:16:51 +0300

    fixtures

---
+++ fixtures/x_clients.fixture.ts
@@ -0,0 +1,47 @@
+import { test as base } from "@playwright/test";
+import { Company } from "../types/company";
+
+const host = "https://x-clients-be.onrender.com";
+const auth = "/auth/login";
+const company = "/company";
+
+type MyFixtureType = {
+  token: string;
+  company: Company;
+};
+
+export const test = base.extend<MyFixtureType>({
+  company: async ({ request }, use) => {
+    const companyInfo: Company = {
+      id: 0, // id пока не знаем
+      isActive: true, // по умолчанию на сервере ставится true
+      name: "Инженерка",
+      description: "курсы для инженеров от инженеров.",
+    };
+
+    const createCompanyResponse = await request.post(host + company, {
+      headers: { "x-client-token": await getToken(request) },
+      data: { name: companyInfo.name, description: companyInfo.description },
+    });
+
+    const body = await createCompanyResponse.json();
+    companyInfo.id = body["id"];
+    use(companyInfo);
+  },
+
+  token: async ({ request }, use) => {
+    use(await getToken(request));
+  },
+});
+
+async function getToken(request): Promise<string> {
+  const loginData = {
+    username: "leonardo",
+    password: "leads",
+  };
+
+  const authResponse = await request.post(host + auth, { data: loginData });
+  const authBody = await authResponse.json();
+  return authBody["userToken"];
+}
+
--- tests/x_clients.spec.ts
+++ tests/x_clients.spec.ts
@@ -1,99 +1,36 @@
-import { APIRequestContext, expect, test } from "@playwright/test";
+import { expect } from "@playwright/test";
+import { test } from "../fixtures/x_clients.fixture";
 
 const host = "https://x-clients-be.onrender.com";
-const auth = "/auth/login";
-const company = "/company";
+const companyPath = "/company";
 const deleteCompany = "/delete";
 
-test("Информация о компании", async ({ request }) => {
-  const companyInfo = await crateCompany(
-    request,
-    "ИнженеркаТех",
-    "курсы для инженеров от инженеров"
-  );
-
-  const response = await request.get(`${host}${company}/${companyInfo["id"]}`);
-  const body = await response.json();
+test("Информация о компании", async ({ request, company }) => {
+  const response = await request.get(`${host}${companyPath}/${company.id}`);
 
   expect(response.status()).toEqual(200);
-  expect(body["id"]).toStrictEqual(companyInfo.id);
-  expect(body["isActive"]).toStrictEqual(companyInfo.isActive);
-  expect(body["name"]).toStrictEqual(companyInfo.name);
-  expect(body["description"]).toStrictEqual(companyInfo.description);
+  expect(await response.json()).toMatchObject(company);
 });
 
-test("Удаление компании", async ({ request }) => {
-  const companyInfo = await crateCompany(
-    request,
-    "ИнженеркаТех",
-    "курсы для инженеров от инженеров"
-  );
-  const token = await getToken(request);
-
+test("Удаление компании", async ({ request, company, token }) => {
   const response = await request.get(
-    `${host}${company}${deleteCompany}/${companyInfo["id"]}`,
-    { headers: { "x-client-token": token }}
+    `${host}${companyPath}${deleteCompany}/${company.id}`,
+    { headers: { "x-client-token": token } }
   );
-  const body = await response.json();
-
-  expect(response.status()).toEqual(200);
-  expect(body["id"]).toStrictEqual(companyInfo.id);
-  expect(body["isActive"]).toStrictEqual(companyInfo.isActive);
-  expect(body["name"]).toStrictEqual(companyInfo.name);
-  expect(body["description"]).toStrictEqual(companyInfo.description);
-});
-
-test("Редактирование компании", async ({ request }) => {
-  const companyInfo = await crateCompany(
-    request,
-    "ИнженеркаТех",
-    "курсы для инженеров от инженеров"
-  );
-  const token = await getToken(request);
-
-  const updatedInfo = {
-    description: `Лучшие ${companyInfo.description}`
-  }
   
-  const response = await request.patch(
-    `${host}${company}/${companyInfo["id"]}`,
-    { headers: { "x-client-token": token }, data: updatedInfo}
-  );
-  const body = await response.json();
-
   expect(response.status()).toEqual(200);
-  expect(body["id"]).toStrictEqual(companyInfo.id);
-  expect(body["isActive"]).toStrictEqual(companyInfo.isActive);
-  expect(body["name"]).toStrictEqual(companyInfo.name);
-  expect(body["description"]).toStrictEqual( `Лучшие ${companyInfo.description}`);
+  expect(await response.json()).toMatchObject(company);
 });
 
-async function crateCompany(
-  request: APIRequestContext,
-  name: string,
-  description: string
-) {
-  const companyInfo = { name: name, description: description };
-  const token = await getToken(request);
-  const createCompanyResponse = await request.post(host + company, {
+test("Редактирование компании", async ({ request, company, token }) => {
+  company.description = `Лучшие ${company.description}`
+
+  const response = await request.patch(`${host}${companyPath}/${company.id}`, {
     headers: { "x-client-token": token },
-    data: companyInfo,
+    data: {description: company.description}
   });
 
-  const body = await createCompanyResponse.json();
+  expect(response.status()).toEqual(200);
+  expect(await response.json()).toMatchObject(company);
+});
 
-  companyInfo["id"] = body["id"];
-  companyInfo["isActive"] = true; // по умолчанию на сервере ставится true
-  return companyInfo;
-}
-
-async function getToken(request: APIRequestContext) {
-  const loginData = {
-    username: "leonardo",
-    password: "leads",
-  };
-
-  const authResponse = await request.post(host + auth, { data: loginData });
-  const authBody = await authResponse.json();
-  return authBody["userToken"];
-}
